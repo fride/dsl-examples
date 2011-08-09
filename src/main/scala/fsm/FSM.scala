de1a96a51760890676e6640245726dce8a6bcbcc
Jan <jan@friderici.net>
2011-08-09 11:14:37 +0200

    Implemented better FSM Dsl. Got idea from http://akka.io/docs/akka/1.1.3/scala/fsm.html

--- src/main/scala/fsm/FSM.scala
+++ src/main/scala/fsm/FSM.scala
@@ -2,50 +2,90 @@ package fsm
 
 import scala.PartialFunction
 import collection.mutable.Map
-import com.sun.xml.internal.xsom.impl.Ref.Term
+import java.lang.IllegalStateException
 
-trait FSM[ST,E] {
+/**
+ * Trait for Classes that need a finte state machine.
+ * @param ST the States type
+ * @param E the events type
+ */
+trait FSM[ST,E] extends PartialFunction[E,ST] {
 
   type StateFunction = PartialFunction[E,ST]
+  type TransitionHandler = PartialFunction[(ST,ST),Unit]
 
   private var currentState:Option[ST] = None
 
   private val transitions:Map[ST,StateFunction] = Map()
   private var terminalStates:Set[ST] = Set()
+  private var transitionHandler:TransitionHandler = {
+    case (from, to) => println("TRansition from " + from + " => " + to)
+  }
 
-  def isTerminated = {
+  def terminated = {
     currentState match {
       case Some(st) => terminalStates.contains(st)
       case _ => false
     }
   }
+
+  /**
+   * Set the start state
+   */
   def startWith(state:ST) = {
     currentState = Some(state)
   }
 
+  /**
+   * Add a terminating state.
+   */
   def stopOn(terminalState:ST) {
      terminalStates += terminalState
   }
+
+  /**
+   * What to do in a given state
+   * @param state the state
+   * @oaram fun The transition.
+   */
   def in(state:ST)(fun:StateFunction) =  {
     transitions.put(state,fun)
   }
 
+  private def nextState(e: E): Option[ST] = {
+    val nextState = for (state <- currentState;
+                         transition <- transitions.get(state);
+                         if transition.isDefinedAt(e)) yield (transition(e))
+    nextState
+  }
+
+  /**
+   *@param e the event to handle.
+   *@return the new state.
+   *@throws IllegalStateException if the event can not be handled.
+   */
   def onEvent(e:E) = {
     println("In " + currentState + " got " + e)
-    val nextState = for (state <- currentState;
-         transition <- transitions.get(state);
-         if transition.isDefinedAt(e)) yield(transition(e))
-    nextState match {
-      case None => println("No Transition found")
-      case Some(st) => enterState(st)
+    nextState(e)  match {
+      case None => throw new IllegalStateException("No Transition for Event: " + e)
+      case Some(st) => enterState(st); st
     }
   }
 
+  def onTransition(fun:TransitionHandler) = {
+    this.transitionHandler = fun
+  }
   protected def enterState(st:ST) = {
     currentState = Some(st)
   }
-}
 
-object FSM {
 
+  def apply(event:E):ST = onEvent(event)
+
+  override def isDefinedAt(event:E) = {
+    nextState(event) match {
+      case Some(st) => true
+      case None => false
+    }
+  }
 }
--- src/test/scala/fsm/FsmTraitSpecs.scala
+++ src/test/scala/fsm/FsmTraitSpecs.scala
@@ -6,46 +6,45 @@ import org.scalatest.matchers.ShouldMatchers
 
 class FsmTraitSpecs extends FlatSpec with ShouldMatchers  {
 
-  sealed trait State
-  object A extends State
-  object B extends State
-  object C extends State
+  sealed trait State {
+    def name:String
+    override def toString = name
+  }
+  object START extends State { def name = "Start"}
+  object J extends State { def name = "J"}
+  object A extends State { def name = "A"}
+  object N extends State { def name = "N"}
+
 
   class ExampleFsm extends FSM[State,String] {
+    in(START) {
+      case "J" => J
+    }
+
+    in(J) {
+      case "A" => A
+    }
+
     in(A) {
-      case "B" =>
-        println("Got B")
-        B
+      case "N" => N
     }
 
-    in(B) {
-      case "C" =>
-        print("Got C")
-        C
-      case "A" =>
-        println("Got A")
-        A
-    }
-
-    stopOn(C)
-
-    startWith(A)
+    stopOn(N)
+    startWith(START)
   }
 
-  "The example DSL shoul" should "Terminate with BC" in {
+  "The example DSL shoul" should "Accecpt \"JAN\"" in {
     val fsm = new ExampleFsm()
-    fsm.onEvent("B")
-    fsm.onEvent("C")
-    fsm.isTerminated should be(true)
-  }
-
-  "The example DSL shoul" should "Terminate with BABC" in {
-    val fsm = new ExampleFsm()
-    fsm.onEvent("B")
+    fsm.onEvent("J")
     fsm.onEvent("A")
-    fsm.onEvent("B")
-    fsm.onEvent("C")
-    fsm.isTerminated should be(true)
+    fsm.onEvent("N")
+    fsm.terminated should be(true)
   }
 
+  "The example DSL" should "Work as a partial function" in {
+    val fsm = new ExampleFsm()
+    val states = ("J" :: "A" :: "N" :: Nil).map(fsm)
+    fsm.terminated should be(true)
+    states should be (List(J,A,N))
+  }
 }
